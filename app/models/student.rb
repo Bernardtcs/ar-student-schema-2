8b9df5d4a992d044b8530465361bf2d18cbfbc05
agweiler <kheryee@live.com>
2015-09-25 15:55:51 +0800

    added attributes for student class to map to column headers added name function to map to first name and last name added age function to map to current time - birthda to first name and last name added age function to map to current time - birthday

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,23 +1,13 @@
 require_relative '../../db/config'
+require 'byebug'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
-self.attribute_names =  [:id, :first_name, :last_name, :gender, :birthday, :phone, :created_at, :updated_at]
-
+  attr_accessor :first_name, :last_name, :gender, :birthday, :email, :phone
   attr_reader :name, :age
 
-  def assign_attributes(attributes={})
-    attributes.symbolize_keys!
-
-    # This defines the value even if it's not present in attributes
-    @attributes = {}
-
-    attribute_names.each do |name|
-      @attributes[name] = attributes[name]
-  end
-
   def name
-    @name = @attributes[first_name] + @attributes[last_name]
+    @name = @first_name + " " +@last_name
   end
 
   def age
