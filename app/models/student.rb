669d6b226f88b6bed25625cd6cc1313fd21e1c0a
agweiler <kheryee@live.com>
2015-09-25 17:22:08 +0800

    added email column to table

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,17 +1,16 @@
 require_relative '../../db/config'
-require 'byebug'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
-  attr_accessor :first_name, :last_name, :gender, :birthday, :email, :phone
-  attr_reader :name, :age
+validates :email, :format => { :with => /\w+[@]\w+[.]\w{1}\w+/}, uniqueness: true
+validates :age, :numericality => { :greater_than_or_equal_to => 5 }
 
   def name
-    @name = @first_name + " " +@last_name
+    "#{first_name} #{last_name}"
   end
 
   def age
     now = Time.now.utc.to_date
-    now.year - @birthday.year - ((now.month > @birthday.month || (now.month == @birthday.month && now.day >= @birthday.day)) ? 0 : 1)
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
   end
 end
